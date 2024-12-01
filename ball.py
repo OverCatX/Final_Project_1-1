bcf35fbd4ec58c2cf483e1c0109bf223ec7eceff
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 01:13:21 +0700

    added draw method

---
+++ ball.py
@@ -0,0 +1,20 @@
+import pygame.draw
+
+
+class Ball:
+    def __init__(self, size:int = 20, x:int = 0, y:int = 0,vx:float = 0, vy: float =0
+                 , color: str='', count: int=1, id: int = 0):
+        self.size = size
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.count = count
+        self.id = id
+        self.screen_width = 800
+        self.screen_height = 600
+
+    def draw(self):
+        display = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.draw.circle(display,self.color, (self.x, self.y), self.size)
