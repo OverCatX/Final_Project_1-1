ec909317254c04ab839bf79ee4a1bbda0af70bdc
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 01:17:13 +0700

    added on_hit_screen_edge() method

--- ball.py
+++ ball.py
@@ -18,3 +18,11 @@ class Ball:
     def draw(self):
         display = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.draw.circle(display,self.color, (self.x, self.y), self.size)
+
+    def on_hit_screen_edge(self):
+        if self.x - self.size < 0 or self.x + self.size > self.screen_width:
+            self.vx = -self.vx
+        if self.y - self.size < 0 or self.y + self.size > self.screen_height:
+            self.vy = -self.vy
+
+
