d81f098ecf51e5e879d48d9cb669ece053486085
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 11:14:17 +0700

    add new update method

--- obj/ball.py
+++ obj/ball.py
@@ -17,6 +17,13 @@ class Ball:
     def draw(self, display):
         pygame.draw.circle(display,self.color, (self.x, self.y), self.size)
 
+    def updates(self, paddle):
+        self.x += self.vx
+        self.y += self.vy
+
+        self.on_hit_screen_edge()
+        self.on_hit_paddle(paddle)
+
     def on_hit_screen_edge(self):
         if self.x - self.size < 0 or self.x + self.size > self.screen_width:
             self.vx = -self.vx
