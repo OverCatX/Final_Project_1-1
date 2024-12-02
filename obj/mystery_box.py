4b4f3f21c70d003dff0ea6ef64d582d2e52e7222
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 10:51:30 +0700

    added init

--- obj/mystery_box.py
+++ obj/mystery_box.py
@@ -1,3 +1,10 @@
 class MysteryBlock:
-    def __init__(self):
-        pass
+    def __init__(self, width:int, height: int, x: int, y: int, vy: float, color: str, id: int):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.vy = vy
+        self.color = color
+        self.id = id
+
