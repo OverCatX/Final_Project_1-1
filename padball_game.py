e8deedf6b05650382977825d1ce83c25f3c43e95
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 11:17:35 +0700

    added init method

--- padball_game.py
+++ padball_game.py
@@ -1,4 +1,14 @@
+import pygame
+
 class PadBallGame:
 
     def __init__(self):
-        pass
+        pygame.init()
+        self.screen_width = 800
+        self.screen_height = 600
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("PadBallGame V.1")
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.font_large = pygame.font.Font(None, 74)
+        self.state = "home"
