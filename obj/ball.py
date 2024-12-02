fa404b0f62dacb7b63b689f2ccdf5b9dc1ee8dea
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 11:07:48 +0700

    modified init and draw method

--- obj/ball.py
+++ obj/ball.py
@@ -2,7 +2,7 @@ import pygame.draw
 
 class Ball:
     def __init__(self, size:int = 20, x:int = 0, y:int = 0,vx:float = 0, vy: float =0
-                 , color: str='', count: int=1, id: int = 0):
+                 , color: str='', count: int=1, id: int = 0, screen_width: int=800, screen_height: int=600):
         self.size = size
         self.x = x
         self.y = y
@@ -11,11 +11,10 @@ class Ball:
         self.color = color
         self.count = count
         self.id = id
-        self.screen_width = 800
-        self.screen_height = 600
+        self.screen_width = screen_width
+        self.screen_height = screen_height
 
-    def draw(self):
-        display = pygame.display.set_mode((self.screen_width, self.screen_height))
+    def draw(self, display):
         pygame.draw.circle(display,self.color, (self.x, self.y), self.size)
 
     def on_hit_screen_edge(self):
