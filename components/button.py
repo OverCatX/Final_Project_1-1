d9c563bd96c8afe9abbd16a6b9d2d0769a12859b
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 11:09:53 +0700

    modified draw method

--- components/button.py
+++ components/button.py
@@ -9,13 +9,11 @@ class Button:
         self.button_color = button_color
         self.font = pygame.font.Font(None, 36)
 
-    def draw(self, screen):
-        """Draw the button on the screen."""
-        pygame.draw.rect(screen, self.button_color, self.rect)
+    def draw(self, display):
+        pygame.draw.rect(display, self.button_color, self.rect)
         text_surface = self.font.render(self.text, True, self.text_color)
         text_rect = text_surface.get_rect(center=self.rect.center)
-        screen.blit(text_surface, text_rect)
+        display.blit(text_surface, text_rect)
 
     def is_clicked(self, mouse_pos):
-        """Check if the button is clicked."""
         return self.rect.collidepoint(mouse_pos)
