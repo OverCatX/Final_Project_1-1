019b5efa06d696cac91c1eafd4c682c8f03dfbb9
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-02 10:55:33 +0700

    added draw method

--- obj/mystery_box.py
+++ obj/mystery_box.py
@@ -1,3 +1,5 @@
+import pygame.draw
+
 class MysteryBlock:
     def __init__(self, width:int, height: int, x: int, y: int, vy: float, color: str, id: int):
         self.width = width
@@ -7,4 +9,9 @@ class MysteryBlock:
         self.vy = vy
         self.color = color
         self.id = id
+        self.screen_width = 800
+        self.screen_height = 600
 
+    def draw(self):
+        display = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.draw.rect(display,self.color, (self.x, self.y), 30, 3)
