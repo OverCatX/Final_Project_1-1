a4398f5f372fc8fbd0b36278aa1ace7972f3da08
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-05 16:27:00 +0700

    added stop_sound() method

--- sounds/sound.py
+++ sounds/sound.py
@@ -1,17 +1,22 @@
+import time
+
 import pygame
 
 
 class Sound:
     def __init__(self):
-        self.hit_wall_sound = pygame.mixer.Sound('hit_wall_sound.wav')
-        self.hit_paddle_sound = pygame.mixer.Sound('hit_paddle_sound.wav')
-        self.background_sound = pygame.mixer.Sound('background_music.wav')
+        self.hit_wall_sound = pygame.mixer.Sound('sounds/hit_wall_sound.wav')
+        self.hit_paddle_sound = pygame.mixer.Sound('sounds/hit_paddle_sound.wav')
+        self.background_sound = pygame.mixer.Sound('sounds/background_music.wav')
 
     def run_background_sound(self):
-        self.background_sound.play(-1)
+        pass
 
     def run_sound(self, sound):
         if sound == 'wall':
-            self.hit_wall_sound.play(maxtime=10)
+            self.hit_wall_sound.play()
         elif sound == 'paddle':
-            self.hit_paddle_sound.play(maxtime=10)
+            self.hit_paddle_sound.play()
+
+    def stop_wall_sound(self):
+        self.hit_wall_sound.stop()
