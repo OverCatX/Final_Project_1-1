7dd3cb8eb3f988ffe69efe906a47c8b85935bc6b
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-12 14:09:18 +0700

    Add speed attributes

--- obj/paddle.py
+++ obj/paddle.py
@@ -1,12 +1,13 @@
 import pygame
 
 class Paddle:
-    def __init__(self, width: int, height: int, x: int, y:int,color: str):
+    def __init__(self, width: int, height: int, x: int, y:int,color: str, speed:int):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         self.color = color
+        self.speed = speed
 
     def draw(self, display):
         pygame.draw.rect(display, self.color, (self.x, self.y, self.width, self.height))
