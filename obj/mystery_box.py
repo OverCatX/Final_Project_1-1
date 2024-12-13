2bccda21192d43c161cfb27fa11ec9f9090fc54b
Bhumipat Kusalatham <overcatpocket@gmail.com>
2024-12-13 17:06:02 +0700

    remove id,vy attributes

--- obj/mystery_box.py
+++ obj/mystery_box.py
@@ -1,16 +1,14 @@
 import pygame.draw
 
 class MysteryBlock:
-    def __init__(self, width:int, height: int, x: int, y: int, vy: float, color: str, id: int):
+    def __init__(self, width:int, height: int, x: int, y: int, color: str):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        self.vy = vy
         self.color = color
-        self.id = id
         self.screen_width = 800
         self.screen_height = 600
 
     def draw(self, display):
-        pygame.draw.rect(display,self.color, (self.x, self.y), 30, 3)
+        pygame.draw.rect(display,self.color, (self.x, self.y, self.width, self.height))
